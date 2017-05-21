9b883819602dbdda0aaec7a3c6101d557b6477fc
yOoMarvin <marvin.messenzehl@gmail.com>
2017-05-21 18:38:30 +0200

    Add the update stuff to the table

--- WeatherTableViewController.swift
+++ WeatherTableViewController.swift
@@ -65,23 +65,25 @@ class WeatherTableViewController: UITableViewController, UISearchBarDelegate {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return forecastData.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        // Configure the cell...
-
+        let weatherObject = forecastData[indexPath.row]
+        
+        cell.textLabel?.text = weatherObject.summary
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
